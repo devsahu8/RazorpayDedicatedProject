e25dc608a1af65331658c494d8ece8c7150d207d
devsahu8 <devsahu7180@gmail.com>
2026-04-05 23:46:42 +0530

    datatbase models added

---
+++ app/models.py
@@ -0,0 +1,26 @@
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from typing import Optional
+from app import db
+from datetime import datetime,timezone
+import uuid
+
+class User(db.Model):
+    id: so.Mapped[int]=so.mapped_column(primary_key=True)
+    username: so.Mapped[str]=so.mapped_column(sa.String(60),index=True,unique=True)
+    email: so.Mapped[str]=so.mapped_column(sa.String(60),index=True,unique=True)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    orders: so.WriteOnlyMapped["Order"]=so.relationship(back_populates="user")
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+    
+class Order(db.Model):
+    id: so.Mapped[int]=so.mapped_column(primary_key=True)
+    ord_id: so.Mapped[str]=so.mapped_column(sa.String(100),index=True,unique=True,default=lambda: str(uuid.uuid4()))
+    amount: so.Mapped[int]=so.mapped_column(sa.Integer)
+    status: so.Mapped[str]=so.mapped_column(sa.String(20),default="created")
+    created_at: so.Mapped[datetime]=so.mapped_column(index=True,default=lambda:datetime.now(timezone.utc))
+    user_id: so.Mapped[int]=so.mapped_column(sa.ForeignKey(User.id),index=True)
+    user: so.Mapped[User]=so.relationship(back_populates="orders")
+    
