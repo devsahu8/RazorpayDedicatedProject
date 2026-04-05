ac6c915be633082e22585e7ee1fb9fab01b9787e
devsahu8 <devsahu7180@gmail.com>
2026-04-06 00:03:08 +0530

    modified models and routes

--- app/models.py
+++ app/models.py
@@ -4,8 +4,11 @@ from typing import Optional
 from app import db
 from datetime import datetime,timezone
 import uuid
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     id: so.Mapped[int]=so.mapped_column(primary_key=True)
     username: so.Mapped[str]=so.mapped_column(sa.String(60),index=True,unique=True)
     email: so.Mapped[str]=so.mapped_column(sa.String(60),index=True,unique=True)
@@ -14,6 +17,16 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
     
 class Order(db.Model):
     id: so.Mapped[int]=so.mapped_column(primary_key=True)
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,14 @@
-from flask import render_template,url_for,redirect
+from flask import render_template,url_for,redirect,flash
 from app.forms import IndexForm,LoginForm
 from app import app
+from flask_login import current_user, login_user,logout_user,login_required
+import sqlalchemy as sa
+from app import db
+from app.models import User,Order
 
 @app.route("/",methods=["GET","POST"])
 @app.route("/index",methods=["GET","POST"])
+@login_required
 def index():
     form=IndexForm()
     if form.validate_on_submit():
@@ -12,7 +17,19 @@ def index():
 
 @app.route("/login",methods=["GET","POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
     form=LoginForm()
     if form.validate_on_submit():
+        user=db.session.scalar(sa.select(User).where(User.username==form.username.data))
+        if user is None or not user.check_password(user.password_hash.data):
+            flash("Username or password is wrong")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for("index"))
     return render_template("login.html",form=form,title="Sign-in")
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
